20439334d86850cd5f6c2f849345934ab3f3addb
Alejandra.D <alejandra.davila.tipan@udla.edu.ec>
2023-10-18 07:56:17 -0500

    main agregado

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
-        Materia materia1 = new Materia();
+        // Creamos un objeto de la clase Estudiante
+        Estudiante estudiante = new Estudiante();
+        Materia materia = new Materia();
 
+        estudiante.ingresarDatos();
 
-        }
+        materia.ingresarDatos(1);
+        materia.ingresarDatos(2);
+        materia.ingresarDatos(3);
+
+        materia.ingresarProfesor(1);
+        materia.ingresarProfesor(2);
+        materia.ingresarProfesor(3);
+
+        estudiante.imprimirMaterias();
+        estudiante.imprimirTodosProfesores();
     }
 }
